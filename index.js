b342767686ad20219234c45d5293f4355144fb02
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-04-19 23:54:12 +0200

    Création d'un test pour le module.exports

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Variables & Constantes nécessaires pour le projet
 const { Client, GatewayIntentBits } = require("discord.js");
 const config = require("./config.json");
 const client = new Client({
@@ -6,8 +7,14 @@ const client = new Client({
     ]
 });
 
+// On connecte le bot
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
-
 client.login(config.token);
+
+// Exécution des modules :
+const monduleTest = require('./commands/test');
+
+monduleTest.logConsole;
+monduleTest.logConsole2;
