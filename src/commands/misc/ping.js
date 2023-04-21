a9a7ac25172dc36ae853ea48d9083aa227d97fff
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-04-21 20:12:31 +0200

    Corrections d'erreurs pour les commentaires

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,9 +1,9 @@
 module.exports = {
     deleted: true, // Supprime la commande pour les utilisateurs
     name: 'ping', // Nom de la commande exécutée
-    description: 'Pong!', // Réponse à la commande
+    description: 'Pong!', // Description de la commande
 
-    callback: (client, interaction) => {
-        interaction.reply(`Pong ! ${client.ws.ping}ms`)
+    callback: (client, interaction) => { // Fonctions pour le message
+        interaction.reply(`Pong ! ${client.ws.ping}ms`) // Equivalent du "return"
     }
 };
